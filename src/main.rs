e2a0f2d6bc20b370a8a5e36f1263ab17342c4cbd
Maakinoh <maakinoh@gmail.com>
2024-03-05 13:31:10 +0100

    Changes to new api

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,67 @@
 use std::error::Error;
 
-use clap::{arg, Command};
+use clap::{arg, Arg, ArgAction, Command};
 
 use clap::crate_authors;
 use regex::Regex;
 use scraper::Html;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ApiResponse{
+    #[serde(rename = "daysRemaining")]
+    days_remaining: i32
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut url: &str = "https://isitfurvesteralready.info/";
+    let mut base_url: &str = "https://isitfurvesteralready.info/";
 
+    let use_scraper = Arg::new("use-scraper")
+        .long("use-scraper")
+        .action(ArgAction::SetTrue)
+        //.value_name("FILE")
+        .help("Uses the old scraper instead of the api");
     let args = Command::new("IsItFurversterAlready")
         .version("1.0")
         .author(crate_authors!("Maakinoh"))
         .about("Requests from https://isitfurvesteralready.info/ if it is furverster already")
         .arg(arg!(--url <VALUE> "URL for a custom Server standard is https://isitfurvesteralready.info/").required(false))
         .arg(arg!(--days "Returns the remaining days until furvester"))
+        //.arg(arg!(--use-scraper ))
+        .arg(use_scraper)
         .get_matches();
 
     let url_arg = args.get_one::<String>("url");
     if url_arg.is_some() {
-        url = url_arg.unwrap();
+        base_url = url_arg.unwrap();
     }
 
-    let response = reqwest::blocking::get(url)?.text()?;
+    // If the old scraper should be used
+    if args.get_flag("use-scraper") {
+        let response = reqwest::blocking::get(base_url)?.text()?;
 
-    let document = scraper::Html::parse_document(&response);
+        let document = scraper::Html::parse_document(&response);
 
-    if !args.get_flag("days") {
-        parse_is_it_furvester(&document);
+        if !args.get_flag("days") {
+            parse_is_it_furvester(&document);
+        } else {
+            parse_remaining_days(&document);
+        }
     } else {
-        parse_remaining_days(&document);
+        // Use the api instead
+
+        let result : ApiResponse = reqwest::blocking::get(base_url.to_owned() +"api/getDaysRemaining")?.json()?;
+        if !args.get_flag("days") {
+            if result.days_remaining > 0 {
+                println!("No");
+            }
+        } else {
+            println!("{} days remaining", result.days_remaining);
+        }
+
     }
+
+
     Ok(())
 }
 
@@ -65,5 +96,3 @@ fn parse_remaining_days(document: &Html) {
         println!("{}", end);
     });
 }
-
-
